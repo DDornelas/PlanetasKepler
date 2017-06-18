f283abf8621d5c1054957520cb79363265476eac
Daneil Dornelas <dornelasdaniel@id.uff.br>
2017-06-18 14:33:55 -0300

    Excentricidade

--- DoisCorpos.py
+++ DoisCorpos.py
@@ -26,11 +26,8 @@ class Planeta:
 		self.vyt += ayt*dt
 		self.r = math.sqrt((self.xt**2)+(self.yt**2))
 		
-G=6.67*10**(-11)
 GM=4*np.pi**2
 dt=0.0001
-
-p1=Planeta(1, 1, 0, 0, 2*np.pi+1)
 
 tmax=2
 t=np.arange(0, tmax, dt)
@@ -41,9 +38,22 @@ vyTerra=np.zeros(t.size)
 rTerra=np.zeros(t.size)
 eTerra=np.zeros(t.size)
 
-for i in range(t.size):
-	p1.move()
-	xTerra[i], yTerra[i], vxTerra[i], vyTerra[i], rTerra[i], eTerra[i] = p1.xt, p1.yt, p1.vxt, p1.vyt, p1.r, p1.et
+z=0
+exc=[]
+v0=[]
+
+while z<1:
+	
+	p1=Planeta(1, 1, 0, 0, 2*np.pi+z)
+	for i in range(t.size):
+		p1.move()
+		xTerra[i], yTerra[i], vxTerra[i], vyTerra[i], rTerra[i], eTerra[i] = p1.xt, p1.yt, p1.vxt, p1.vyt, p1.r, p1.et
+	
+	a=(math.sqrt((max(xTerra)**2)+(min(xTerra)**2))/2)
+	b=(math.sqrt((max(yTerra)**2)+(min(yTerra)**2))/2)
+	exc.append(math.sqrt(1-((b**2)/(a**2))))
+	v0.append(z)
+	z+=0.1
 	
 plt.figure(figsize=(6,5), dpi=96)
 
@@ -55,13 +65,11 @@ plt.axes().set_aspect('equal','datalim')
 
 plt.rc('text', usetex=True)
 plt.rc('font', **{'sans-serif' : 'Arial', 'family' : 'sans-serif'})
-plt.xlabel(r'$Raio_{(t)}$(m)')
-plt.ylabel(r'$Vx_{(t)}$(m)')
+plt.xlabel(r'Excentricidade')
+plt.ylabel(r'$Vx_{(0)}(\frac{AU}{Ano})$')
 
-plt.title(r'Espa\c{c}o de Fases Terra Sol Velocidade Inicial 2$\pi$+1',fontsize=12)
+plt.title(r'Excentricidade Terra Sol',fontsize=12)
 plt.grid()
-plt.plot(rTerra, vxTerra,'r-', linewidth=1)
-plt.savefig("EFR1.pdf", dpi=96)
+plt.plot(v0, exc,'r-', linewidth=1)
+plt.savefig("EXC.pdf", dpi=96)
 plt.show()
-	
-
